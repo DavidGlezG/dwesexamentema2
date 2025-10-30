f0a482f81c709829aa6628745e851a6be7f0e0c4
David <davidglezg5@gmail.com>
2025-10-30 11:53:22 +0100

    Clase artículo realizada

--- model/Articulo.php
+++ model/Articulo.php
@@ -1,2 +1,20 @@
 <?php
 // Clase Artículo
+class Articulo
+{
+    // Declarar atributos públicos
+    public string $nombre;
+    public float $precio;
+    public bool $disponibilidad;
+    public string $categoría;
+
+    // Crear el constructor
+    public function __construct(string $nombre, float $precio, bool $disponibilidad, string $categoría)
+    {
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponibilidad = $disponibilidad;
+        $this->categoría = $categoría;
+    }
+}
+?>
